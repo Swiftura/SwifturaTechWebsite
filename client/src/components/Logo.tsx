76ac9f1ef1a24f15b5777d7cc0618a34ab17caa9
maser0602 <42018831-maser0602@users.noreply.replit.com>
2025-05-07 18:45:13 +0000

    8b47cecf

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import logoImage from "@/assets/swiftura-logo.png";
 const Logo = () => {
   return (
     <a href="#" className="text-white font-bold text-2xl flex items-center">
-      <div className="h-12 flex items-center mr-3">
+      <div className="h-12 flex items-center mr-1">
         <img src={logoImage} alt="Swiftura Logo" className="h-full w-auto" />
       </div>
       <span className="tracking-wide">Swiftura</span>
